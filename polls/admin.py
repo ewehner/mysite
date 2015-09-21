633d424da777ca49d820f63764c45f7855338053
Elizabeth Wehner <ewehner@gmail.com>
2015-09-21 12:22:53 -0500

    Push initial repository to github
    Django App up through writing views, before simplifying views.

---
+++ polls/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Choice, Question
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Question', {'fields': ['question_text']}),
+	('Date Information', {'fields': ['pub_date'], 'classes': ['expand']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
